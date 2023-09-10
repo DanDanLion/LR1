c874885206654769cfd9054106894fe1ad3d7b34
LR1 <nickita.sosnovskiy05@gmail.com>
2023-09-10 13:11:54 +0300

    Laborator Work 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import javax.print.attribute.standard.DateTimeAtProcessing;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
         System.out.println("Hello and welcome!");
         System.out.println("Нікіта Сосновський ПЗ-20-1/9");
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = currentDate.format(formatter);
+
+        System.out.println("Поточна дата: " + formattedDate);
     }
 }
